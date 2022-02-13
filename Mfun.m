a2d7aeb60a49ad3165dc5886953855dddee4d384
negregr <oxygen1u8@mail.ru>
2022-02-13 10:06:22 +0300

    Mfun ready. First three task done

--- Mfun.m
+++ Mfun.m
@@ -1,3 +1,15 @@
 function retval = Mfun (A, C)
-    
+    retval = [];
+    L = 2^(size(C)(2)) - 1;
+    for i = 1:L
+        X = A.*C;
+        nextval = mod(sum(X), 2);
+        if A(size(A)(2)) == 0
+            retval = [1 retval];
+        else
+            retval = [-1 retval];
+        end
+        A = [nextval A(1:size(A)(2) - 1)];
+    end
+    retval = fliplr(retval);
 endfunction
--- lab1.m
+++ lab1.m
@@ -1,6 +1,24 @@
+close all;
+clear all;
+
 A  = [1 0 0 0 0 1 0 0 1 0];
 C1 = [1 0 0 1 0 0 1 1 1 1];
 C2 = [1 0 0 1 0 1 1 0 1 1];
 
 M1 = Mfun(A, C1);
 M2 = Mfun(A, C2);
+
+x = 1:size(M1)(2);
+subplot(3, 1, 1);
+grid on;
+plot(x, M1)
+subplot(3, 1, 2);
+grid on;
+plot(x, M2)
+
+shift = 100 + 18*10;
+M = [M1 zeros(1, shift)] - [zeros(1, shift) M2];
+subplot(3, 1, 3);
+grid on
+x = 1:size(M)(2);
+plot(x, M);
