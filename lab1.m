eae2418c225e3c183ca3a92561b7741df002b4a9
oxygen1u8 <egors5779@gmail.com>
2022-02-14 12:51:33 +0300

    my_sf.m done. Task 4 and 5 has been done too

--- lab1.m
+++ lab1.m
@@ -8,17 +8,32 @@ C2 = [1 0 0 1 0 1 1 0 1 1];
 M1 = Mfun(A, C1);
 M2 = Mfun(A, C2);
 
-x = 1:size(M1)(2);
+x = 0:size(M1)(2) - 1;
 subplot(3, 1, 1);
-grid on;
 plot(x, M1)
+title('M_1');
+
 subplot(3, 1, 2);
-grid on;
 plot(x, M2)
+title('M_2');
 
 shift = 100 + 18*10;
 M = [M1 zeros(1, shift)] - [zeros(1, shift) M2];
 subplot(3, 1, 3);
-grid on
-x = 1:size(M)(2);
+x = 0:size(M)(2) - 1;
 plot(x, M);
+title('M_1 - ~M_2');
+
+figure
+
+acf = my_sf(M1, M1);
+x = 0:1:length(acf)-1;
+subplot(4, 2, 1);
+plot(x, acf);
+title('my\_sf(M_1, M_1)');
+
+ccf = my_sf(M2, M1);
+x = 0:1:length(acf)-1;
+subplot(4, 2, 3);
+plot(x, ccf);
+title('my\_sf(M_2, M_1)');
--- my_sf.m
+++ my_sf.m
@@ -1,3 +1,9 @@
 function retval = my_sf (A, B)
-    
+    retval = [];
+    L = length(A) + length(B);
+    B_ = [zeros(1, length(A)) B zeros(1, length(A))];
+    for i = 0:L-1
+        A_ = [zeros(1, L - i) A zeros(1, i)];
+        retval(end + 1) = sum(B_.*A_);
+    endfor
 endfunction
