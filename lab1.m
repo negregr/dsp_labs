95e03d3717060719a35d361835071d64b77569c1
oxygen1u8 <egors5779@gmail.com>
2022-02-14 13:13:46 +0300

    The 1st part of 1st lab is already done!

--- lab1.m
+++ lab1.m
@@ -37,3 +37,60 @@ x = 0:1:length(acf)-1;
 subplot(4, 2, 3);
 plot(x, ccf);
 title('my\_sf(M_2, M_1)');
+
+ccf = my_sf(M, M1);
+x = 0:1:length(ccf)-1;
+subplot(4, 2, 5);
+plot(x, ccf);
+title('my\_sf(M_s_u_m_m, M_1)');
+
+ccf = my_sf(M, M2);
+x = 0:1:length(ccf)-1;
+subplot(4, 2, 7);
+plot(x, ccf);
+title('my\_sf(M_s_u_m_m, M_2)');
+
+acf = xcorr(M1, M1);
+x = 0:1:length(acf)-1;
+subplot(4, 2, 2);
+plot(x, acf);
+title('xcorr(M_1, M_1)');
+
+ccf = xcorr(M2, M1);
+x = 0:1:length(acf)-1;
+subplot(4, 2, 4);
+plot(x, ccf);
+title('xcorr(M_2, M_1)');
+
+ccf = xcorr(M, M1);
+x = 0:1:length(ccf)-1;
+subplot(4, 2, 6);
+plot(x, ccf);
+title('xcorr(M_s_u_m_m, M_1)');
+
+ccf = xcorr(M, M2);
+x = 0:1:length(ccf)-1;
+subplot(4, 2, 8);
+plot(x, ccf);
+title('xcorr(M_s_u_m_m, M_2)');
+
+A_noise = 18;
+noise = A_noise - 2*A_noise*rand(1, length(M1));
+M3 = M1 + noise;
+
+figure
+subplot(3, 1, 1)
+x = 0:1:length(M3) - 1;
+plot(x, M3)
+title('M_3');
+
+subplot(3, 1, 2)
+ccf = my_sf(M3, M1);
+x = 0:1:length(ccf)-1;
+plot(x, ccf)
+title('my\_sf(M_3, M_1)')
+
+subplot(3, 1, 3)
+ccf = 20*log10(abs(ccf/max(ccf)));
+plot(x, ccf)
+title('my\_sf(M_3, M_1), dB')
