90b0c30a8bcc566feb530fdc1f09a60a26065a73
oxygen1u8 <egors5779@gmail.com>
2022-02-27 22:04:43 +0300

    Wfun has been done

--- Wfun.m
+++ Wfun.m
@@ -1,6 +1,6 @@
 function retval = Wfun (data, r)
+    retval = [];
     data_b = [];
-    L = 2^(r - 2);
     if data ~= 0
         while data > 0
             data_b(end + 1) = mod(data, 2);
@@ -10,4 +10,15 @@ function retval = Wfun (data, r)
         data_b = data;
     end
     data_b = [zeros(1, r - length(data_b)) fliplr(data_b)];
+    T = zeros(1, 2^(r-2));
+    retval(end + 1) = 0;
+    for i=1:r-1
+        curr_index = 2^(i - 1);
+        for j = curr_index:2^(i) - 1
+            nextvalue = mod(T(curr_index) + data_b(i + 1), 2);
+            T = [nextvalue T(1:length(T) - 1)];
+            retval(end + 1) = nextvalue;
+        endfor
+    endfor
+    retval = -2.*retval + ones(1, length(retval));
 endfunction
--- lab1_2.m
+++ lab1_2.m
@@ -1,4 +1,11 @@
+clear all;
+close all;
+
 data1 = 18;
 data2 = 378;
 r     = 10;
 
+W1 = Wfun(data1, r);
+W2 = Wfun(data2, r);
+
+Wsum = W1 + W2;
